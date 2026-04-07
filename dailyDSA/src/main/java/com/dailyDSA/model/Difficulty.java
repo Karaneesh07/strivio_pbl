63fde050c612e6541ec5dcdb5dcd6f992bf54b7b
Karaneesh A <163381366+Karaneesh07@users.noreply.github.com>
2026-04-07 07:05:34 +0530

    Delete dailyDSA directory

--- dailyDSA/src/main/java/com/dailyDSA/model/Difficulty.java
+++
@@ -1,7 +0,0 @@
-package com.dailyDSA.model;
-
-public enum Difficulty {
-    EASY,
-    MEDIUM,
-    HARD
-}
--- dailyDSA/src/main/java/com/dailyDSA/model/SubmissionStatus.java
+++
@@ -1,7 +0,0 @@
-package com.dailyDSA.model;
-
-public enum SubmissionStatus {
-    ACCEPTED,
-    REJECTED,
-    PENDING
-}
